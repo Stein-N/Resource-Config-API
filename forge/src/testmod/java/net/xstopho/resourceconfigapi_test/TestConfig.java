20ea8aced8ed6415344fa85220fa319fc33866a3
Stein-N <nico-stein-98@outlook.de>
2024-07-20 12:45:13 +0200

    neo/forge: fixing TestConfig

--- forge/src/testmod/java/net/xstopho/resourceconfigapi_test/TestConfig.java
+++ forge/src/testmod/java/net/xstopho/resourceconfigapi_test/TestConfig.java
@@ -15,12 +15,12 @@ public class TestConfig {
 
     public static final ResourceConfigBuilder BUILDER = new ResourceConfigBuilder();
 
-    public static final ConfigEntry<Integer> NORMAL_INTEGER, RANGED_INTEGER;
-    public static final ConfigEntry<Double> NORMAL_DOUBLE, RANGED_DOUBLE;
-    public static final ConfigEntry<Boolean> BOOLEAN;
-    public static final ConfigEntry<String> NORMAL_STRING, RANGED_STRING;
-    public static final ConfigEntry<List<String>> LIST;
-    public static final ConfigEntry<ArrayList<String>> ARRAY_LIST;
+    public static final Supplier<Integer> NORMAL_INTEGER, RANGED_INTEGER;
+    public static final Supplier<Double> NORMAL_DOUBLE, RANGED_DOUBLE;
+    public static final Supplier<Boolean> BOOLEAN;
+    public static final Supplier<String> NORMAL_STRING, RANGED_STRING;
+    public static final Supplier<List<String>> LIST;
+    public static final Supplier<ArrayList<String>> ARRAY_LIST;
 
     private static final List<String> ITEM_LIST = Arrays.asList(Items.DIAMOND.toString(), Items.DIAMOND_BLOCK.toString());
     private static final ArrayList<String> BLOCK_LIST = new ArrayList<>(List.of(Blocks.ANDESITE.toString(), Blocks.DIAMOND_ORE.toString()));
