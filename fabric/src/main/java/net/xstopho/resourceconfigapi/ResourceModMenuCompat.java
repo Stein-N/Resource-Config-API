f427b74f5b2d1cbea9e94580931fd4ccfcdb540d
Stein-N <nico-stein-98@outlook.de>
2024-10-15 18:55:28 +0200

    fabric: all registered Configs get added to ModMenu

--- fabric/src/main/java/net/xstopho/resourceconfigapi/ResourceModMenuCompat.java
+++ fabric/src/main/java/net/xstopho/resourceconfigapi/ResourceModMenuCompat.java
@@ -2,7 +2,12 @@ package net.xstopho.resourceconfigapi;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.xstopho.resourceconfigapi.api.ConfigRegistry;
+import net.xstopho.resourceconfigapi.api.ConfigType;
+import net.xstopho.resourceconfigapi.config.ResourceModConfig;
+import net.xstopho.resourceconfigapi.gui.screen.ResourceConfigScreen;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResourceModMenuCompat implements ModMenuApi {
@@ -10,7 +15,12 @@ public class ResourceModMenuCompat implements ModMenuApi {
     //TODO: correct implementation when gui is finished
     @Override
     public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
-        // String modId, ScreenFactory
-        return Map.of();
+        Map<String, ConfigScreenFactory<?>> configScreenFactoryMap = new HashMap<>();
+
+        for (Map.Entry<String, Map<ConfigType, ResourceModConfig>> entry : ConfigRegistry.getModConfigFiles().entrySet()) {
+            configScreenFactoryMap.put(entry.getKey(), screen -> new ResourceConfigScreen(screen, entry.getKey()));
+        }
+
+        return configScreenFactoryMap;
     }
 }
