eac8a1e7977f0dacaa6f71484afacd446adcba7c
Stein-N <nico-stein-98@outlook.de>
2024-10-18 21:27:39 +0200

    common: additional AbstractWidgets added by Datatype specific ValueConfigEntries get rendered at Base Level

--- common/src/main/java/net/xstopho/resourceconfigapi/gui/widgets/entries/ValueConfigEntry.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/gui/widgets/entries/ValueConfigEntry.java
@@ -1,13 +1,15 @@
 package net.xstopho.resourceconfigapi.gui.widgets.entries;
 
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.SpriteIconButton;
-import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.components.*;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.xstopho.resourceconfigapi.ResourceConfigConstants;
 import net.xstopho.resourceconfigapi.config.entry.ConfigEntry;
+import net.xstopho.resourceconfigapi.util.ComponentCreator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class ValueConfigEntry<T> extends ConfigBaseEntry {
 
@@ -16,9 +18,9 @@ public abstract class ValueConfigEntry<T> extends ConfigBaseEntry {
     protected final SpriteIconButton undo;
     protected final Button reset;
 
-    public int BASE_WIDGET_WIDTH = 150;
-    public int FINAL_WIDGET_WIDTH;
+    public int WIDGET_WIDTH = 150;
 
+    protected List<AbstractWidget> valueWidgets = new ArrayList<>();
 
     public ValueConfigEntry(ConfigEntry<T> configEntry) {
         this.configEntry = configEntry;
@@ -34,13 +36,16 @@ public abstract class ValueConfigEntry<T> extends ConfigBaseEntry {
 
         this.children.add(undo);
         this.children.add(reset);
-
-        FINAL_WIDGET_WIDTH = BASE_WIDGET_WIDTH - (undo.getWidth() + reset.getWidth()) - 1;
     }
 
     @Override
     public void render(GuiGraphics guiGraphics, int index, int yPos, int xPos, int entryWidth, int entryHeight,
                        int mouseX, int mouseY, boolean hovered, float partialTick) {
+
+        drawStringWithTooltip(guiGraphics, ComponentCreator.label(configEntry.getPath()),
+                ComponentCreator.tooltip(configEntry.getPath()),
+                xPos, yPos, mouseX, mouseY, hovered);
+
         undo.setX(xPos + entryWidth - undo.getWidth() - reset.getWidth());
         undo.setY(yPos);
 
@@ -49,6 +54,20 @@ public abstract class ValueConfigEntry<T> extends ConfigBaseEntry {
 
         undo.render(guiGraphics, mouseX, mouseY, partialTick);
         reset.render(guiGraphics, mouseX, mouseY, partialTick);
+
+        for (AbstractWidget widget : valueWidgets) {
+            widget.setX(xPos + entryWidth - WIDGET_WIDTH);
+            if (widget instanceof EditBox) widget.setY(yPos + 1);
+            else widget.setY(yPos);
+            widget.setWidth(WIDGET_WIDTH - (undo.getWidth() + reset.getWidth()) - 1);
+
+            widget.render(guiGraphics, mouseX, mouseY, partialTick);
+        }
+    }
+
+    protected void add(AbstractWidget widget) {
+        this.children.add(widget);
+        this.valueWidgets.add(widget);
     }
 
     protected void setUndo(boolean active) {
