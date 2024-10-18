bd9c83d02ac0145688056538f15aefa9c9449a32
Stein-N <nico-stein-98@outlook.de>
2024-10-18 22:47:43 +0200

    common: adding CategoryEntries and normal Entries automatically and in correct order

--- common/src/main/java/net/xstopho/resourceconfigapi/gui/widgets/ResourceCategoryTab.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/gui/widgets/ResourceCategoryTab.java
@@ -1,12 +1,19 @@
 package net.xstopho.resourceconfigapi.gui.widgets;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.tabs.Tab;
 import net.minecraft.client.gui.navigation.ScreenRectangle;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.xstopho.resourceconfigapi.config.ResourceModConfig;
+import net.xstopho.resourceconfigapi.config.entry.ConfigEntry;
+import net.xstopho.resourceconfigapi.gui.widgets.entries.CategoryEntry;
+import net.xstopho.resourceconfigapi.gui.widgets.entries.ConfigBaseEntry;
+import net.xstopho.resourceconfigapi.util.CategoryTabHelper;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ResourceCategoryTab implements Tab {
@@ -14,9 +21,31 @@ public class ResourceCategoryTab implements Tab {
     private final ResourceModConfig config;
     private final Screen previous;
 
+    protected final ResourceCategoryEntryList entryList;
+    private final List<ConfigBaseEntry> entries = new ArrayList<>();
+
     public ResourceCategoryTab(Screen previous, ResourceModConfig config) {
         this.previous = previous;
         this.config = config;
+
+        String category = null;
+        for (ConfigEntry<?> entry : config.getBuilder().getEntries().values()) {
+            String path = entry.getPath();
+
+            if (path.contains(".")) {
+                String tempCategory = path.split("\\.")[0];
+
+                if (!tempCategory.equalsIgnoreCase(category)) {
+                    category = tempCategory;
+                    entries.add(new CategoryEntry(category));
+                }
+            }
+
+            entries.add(CategoryTabHelper.createWidget(entry));
+        }
+
+        entryList = new ResourceCategoryEntryList(Minecraft.getInstance(), this.previous.width,
+                this.previous.height, 0, 24, entries);
     }
 
     @Override
@@ -30,12 +59,12 @@ public class ResourceCategoryTab implements Tab {
 
     @Override
     public void visitChildren(Consumer<AbstractWidget> consumer) {
-
+        consumer.accept(entryList);
     }
 
     @Override
     public void doLayout(ScreenRectangle screenRectangle) {
-
+        this.entryList.setRectangle(screenRectangle.width(), screenRectangle.height(), screenRectangle.left(), screenRectangle.top());
     }
 
     public Screen getPreviousScreen() {
