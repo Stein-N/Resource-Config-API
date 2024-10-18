84f4095c9701ec70023b5fc4c08da0f6e02cad77
Stein-N <nico-stein-98@outlook.de>
2024-10-18 23:09:56 +0200

    common: removing reference datatypes for now

--- common/src/main/java/net/xstopho/resourceconfigapi/example_configs/ExampleCommonConfig.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/example_configs/ExampleCommonConfig.java
@@ -19,20 +19,10 @@ public class ExampleCommonConfig {
     public static final ConfigEntry<Integer> INTEGER, CATEGORY_INTEGER;
     public static final ConfigEntry<Long> LONG, CATEGORY_LONG;
     public static final ConfigEntry<Short> SHORT, CATEGORY_SHORT;
-    public static final ConfigEntry<String> STRING, CATEGORY_STRING;
-    public static final ConfigEntry<ConfigType> ENUM, CATEGORY_ENUM;
+    //public static final ConfigEntry<String> STRING, CATEGORY_STRING;
+    //public static final ConfigEntry<ConfigType> ENUM, CATEGORY_ENUM;
 
-    public static final ConfigEntry<Byte[]> BYTE_ARRAY;
-    public static final ConfigEntry<Character[]> CHARACTER_ARRAY;
-    public static final ConfigEntry<Double[]> DOUBLE_ARRAY;
-    public static final ConfigEntry<Float[]> FLOAT_ARRAY;
-    public static final ConfigEntry<Integer[]> INTEGER_ARRAY;
-    public static final ConfigEntry<Long[]> LONG_ARRAY;
-    public static final ConfigEntry<Short[]> SHORT_ARRAY;
-    public static final ConfigEntry<String[]> STRING_ARRAY;
-    public static final ConfigEntry<ConfigType[]> ENUM_ARRAY;
-
-    public static final ConfigEntry<List<Integer>> INTEGER_LIST, CATEGORY_INTEGER_LIST;
+    //public static final ConfigEntry<List<Integer>> INTEGER_LIST, CATEGORY_INTEGER_LIST;
 
     static {
         BOOLEAN = BUILDER.define("boolean", true);
@@ -43,9 +33,9 @@ public class ExampleCommonConfig {
         INTEGER = BUILDER.define("integer", 100);
         LONG = BUILDER.define("long", 983649816749124L);
         SHORT = BUILDER.define("short", (short) 345);
-        STRING = BUILDER.define("string", "Hello World!");
-        INTEGER_LIST = BUILDER.define("integerList", integerList);
-        ENUM = BUILDER.define("enum", ConfigType.CLIENT);
+        //STRING = BUILDER.define("string", "Hello World!");
+        //INTEGER_LIST = BUILDER.define("integerList", integerList);
+        //ENUM = BUILDER.define("enum", ConfigType.CLIENT);
 
         BUILDER.push("Category");
 
@@ -53,24 +43,15 @@ public class ExampleCommonConfig {
         CATEGORY_BYTE = BUILDER.define("byte", (byte) 34);
         CATEGORY_CHARACTER = BUILDER.define("character", 'l');
         CATEGORY_DOUBLE = BUILDER.define("double", 3.8);
+
+        BUILDER.pop().push("Kategorie");
+
         CATEGORY_FLOAT = BUILDER.define("float", 5.39874f);
         CATEGORY_INTEGER = BUILDER.define("integer", 388);
         CATEGORY_LONG = BUILDER.define("long", 238745L);
         CATEGORY_SHORT = BUILDER.define("short", (short) 457);
-        CATEGORY_STRING = BUILDER.define("string", "Category String");
-        CATEGORY_ENUM = BUILDER.define("enum", ConfigType.COMMON);
-        CATEGORY_INTEGER_LIST = BUILDER.define("integerList", integerList);
-
-        BUILDER.pop();
-
-        BYTE_ARRAY = BUILDER.define("byteArray", new Byte[]{23, 65, 78});
-        CHARACTER_ARRAY = BUILDER.define("characterArray", new Character[]{'l', 'ä', 'f'});
-        DOUBLE_ARRAY = BUILDER.define("doubleArray", new Double[]{354.435, 3.9, 348.0});
-        FLOAT_ARRAY = BUILDER.define("floatArray", new Float[]{35.35f, 213124.356f, 0.14896124f});
-        INTEGER_ARRAY = BUILDER.define("integerArray", new Integer[]{1, 2, 3, 4, 5});
-        LONG_ARRAY = BUILDER.define("longArray", new Long[]{1234532L, 1249612L, 314612L});
-        SHORT_ARRAY = BUILDER.define("shortArray", new Short[]{(short) 213, (short) 124, (short) 2355});
-        STRING_ARRAY = BUILDER.define("stringArray", new String[]{"Audi", "Porsche", "Mercedes"});
-        ENUM_ARRAY = BUILDER.define("enumArray", new ConfigType[]{ConfigType.COMMON, ConfigType.CLIENT, ConfigType.SERVER});
+        //CATEGORY_STRING = BUILDER.define("string", "Category String");
+        //CATEGORY_ENUM = BUILDER.define("enum", ConfigType.COMMON);
+        //CATEGORY_INTEGER_LIST = BUILDER.define("integerList", integerList);
     }
 }
