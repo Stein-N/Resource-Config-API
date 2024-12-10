03ca87acb5ae6cd27e6db36b4f64024471329376
Stein-N <nico-stein-98@outlook.de>
2024-12-10 19:26:52 +0100

    log which config file gets registered

--- common/src/main/java/net/xstopho/resourceconfigapi/api/ConfigRegistry.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/api/ConfigRegistry.java
@@ -1,5 +1,6 @@
 package net.xstopho.resourceconfigapi.api;
 
+import net.xstopho.resourceconfigapi.ResourceConfigConstants;
 import net.xstopho.resourceconfigapi.annotations.Config;
 import net.xstopho.resourceconfigapi.config.ModConfig;
 import net.xstopho.resourceconfigapi.util.ConfigType;
@@ -8,6 +9,9 @@ public class ConfigRegistry {
 
     public static ModConfig register(Class<?> clazz, ConfigType type, String modId) {
         if (clazz.isAnnotationPresent(Config.class)) {
+            Config config = clazz.getAnnotation(Config.class);
+
+            ResourceConfigConstants.LOG.info("Register config '{}' for mod '{}'", config.fileName(), modId);
             return new ModConfig(clazz, type, modId);
         }
         throw new IllegalArgumentException("You try to register an class that isn't flagged as a Config!");
