25ab59230f0ec1e3649562925baadb0cbd5f5128
Stein-N <nico-stein-98@outlook.de>
2024-12-11 13:31:54 +0100

    adding forge networking

--- forge/src/main/java/net/xstopho/resourceconfigapi/ResourceConfig.java
+++ forge/src/main/java/net/xstopho/resourceconfigapi/ResourceConfig.java
@@ -1,8 +1,10 @@
 package net.xstopho.resourceconfigapi;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.SimpleChannel;
+import net.xstopho.resourceconfigapi.network.ConfigNetwork;
 
 @Mod(Constants.MOD_ID)
 public class ResourceConfig {
@@ -10,7 +12,10 @@ public class ResourceConfig {
     public static SimpleChannel NETWORK;
 
     public ResourceConfig(FMLJavaModLoadingContext context) {
-
+        context.getModEventBus().addListener(this::initNetwork);
     }
 
+    private void initNetwork(FMLCommonSetupEvent event) {
+        event.enqueueWork(ConfigNetwork::initPayloads);
+    }
 }
---
+++ forge/src/main/java/net/xstopho/resourceconfigapi/network/ConfigNetwork.java
@@ -0,0 +1,26 @@
+package net.xstopho.resourceconfigapi.network;
+
+import net.minecraftforge.network.Channel;
+import net.minecraftforge.network.ChannelBuilder;
+import net.minecraftforge.network.SimpleChannel;
+import net.xstopho.resourceconfigapi.Constants;
+import net.xstopho.resourceconfigapi.ResourceConfig;
+import net.xstopho.resourceconfigapi.network.server.SyncConfigPayload;
+
+public class ConfigNetwork {
+
+    public static void initPayloads() {
+        SimpleChannel channel = ChannelBuilder.named(Constants.of("config_network"))
+                .acceptedVersions(Channel.VersionTest.exact(1))
+                .networkProtocolVersion(1)
+                .simpleChannel();
+        ResourceConfig.NETWORK = channel;
+
+        channel.messageBuilder(SyncConfigPayload.class)
+                .encoder(SyncConfigPayload::encode)
+                .decoder(SyncConfigPayload::decode)
+                .consumerNetworkThread(SyncConfigPayload::handle)
+                .add();
+
+    }
+}
