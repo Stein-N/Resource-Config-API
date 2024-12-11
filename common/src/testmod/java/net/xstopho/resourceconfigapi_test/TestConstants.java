934650cc9893c9bc8744760e4556915bbc8ff8ae
Stein-N <nico-stein-98@outlook.de>
2024-12-11 06:23:53 +0100

    remove ConfigUtils serialization wasn't working

--- common/src/testmod/java/net/xstopho/resourceconfigapi_test/TestConstants.java
+++ common/src/testmod/java/net/xstopho/resourceconfigapi_test/TestConstants.java
@@ -1,7 +1,6 @@
 package net.xstopho.resourceconfigapi_test;
 
 import net.xstopho.resourceconfigapi.api.ConfigRegistry;
-import net.xstopho.resourceconfigapi.util.ConfigUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +14,6 @@ public class TestConstants {
         ConfigRegistry.register(TestConfigs.CommonConfig.class, MOD_ID);
         ConfigRegistry.register(TestConfigs.ClientConfig.class, MOD_ID);
         ConfigRegistry.register(TestConfigs.ServerConfig.class, MOD_ID);
-
-        LOG.error("Common config serializable: {}", ConfigUtils.isSerializable(TestConfigs.CommonConfig.class));
-        LOG.error("Client config serializable: {}", ConfigUtils.isSerializable(TestConfigs.ClientConfig.class));
-        LOG.error("Server config serializable: {}", ConfigUtils.isSerializable(TestConfigs.ServerConfig.class));
     }
 
     public static void clientInit() {
