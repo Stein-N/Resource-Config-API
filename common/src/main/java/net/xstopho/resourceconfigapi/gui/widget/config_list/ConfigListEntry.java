54179fe3b2a99c4d9644e618088a1a51a0f3f668
Stein-N <nico-stein-98@outlook.de>
2024-12-13 18:58:15 +0100

    move to config_list package

--- common/src/main/java/net/xstopho/resourceconfigapi/gui/widget/config_list/ConfigListEntry.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/gui/widget/config_list/ConfigListEntry.java
@@ -1,16 +1,20 @@
-package net.xstopho.resourceconfigapi.gui.widget;
+package net.xstopho.resourceconfigapi.gui.widget.config_list;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.minecraft.network.chat.Component;
 import net.xstopho.resourceconfigapi.annotations.Config;
+import net.xstopho.resourceconfigapi.gui.widget.value_list.ValueListWidget;
 import net.xstopho.resourceconfigapi.util.ConfigUtils;
 
 public class ConfigListEntry extends ObjectSelectionList.Entry<ConfigListEntry> {
 
+    private final ValueListWidget valueListWidget;
     private final Component fileName;
 
-    public ConfigListEntry(Class<?> clazz) {
+    public ConfigListEntry(Class<?> clazz, ValueListWidget valueListWidget) {
+        this.valueListWidget = valueListWidget;
+
         Config annotation = clazz.getAnnotation(Config.class);
         this.fileName = Component.literal(annotation.fileName());
     }
--- common/src/main/java/net/xstopho/resourceconfigapi/gui/widget/config_list/ConfigListWidget.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/gui/widget/config_list/ConfigListWidget.java
@@ -1,21 +1,25 @@
-package net.xstopho.resourceconfigapi.gui.widget;
+package net.xstopho.resourceconfigapi.gui.widget.config_list;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.xstopho.resourceconfigapi.config.ModConfig;
+import net.xstopho.resourceconfigapi.gui.widget.value_list.ValueListWidget;
 
 import java.util.List;
 
 public class ConfigListWidget extends ObjectSelectionList<ConfigListEntry> {
 
-    public ConfigListWidget(Minecraft client, int width, int height, int yPos, int itemHeight, List<ModConfig> configList) {
+    private final ValueListWidget valueListWidget;
+
+    public ConfigListWidget(Minecraft client, int width, int height, int yPos, int itemHeight, List<ModConfig> configList, ValueListWidget valueListWidget) {
         super(client, width, height, yPos, itemHeight);
+        this.valueListWidget = valueListWidget;
 
         configList.forEach(this::addConfigEntries);
     }
 
     private void addConfigEntries(ModConfig config) {
-        this.addEntry(new ConfigListEntry(config.getClazz()));
+        this.addEntry(new ConfigListEntry(config.getClazz(), this.valueListWidget));
     }
 
     @Override
