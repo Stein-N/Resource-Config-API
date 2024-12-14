f0f9f6c4aea5a799a2d8eb0e177c677b0d8e2f04
Stein-N <nico-stein-98@outlook.de>
2024-12-14 15:52:03 +0100

    save the ConfigListEntries in an extra List to get them later for global actions

--- common/src/main/java/net/xstopho/resourceconfigapi/gui/widget/config_list/ConfigListWidget.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/gui/widget/config_list/ConfigListWidget.java
@@ -5,21 +5,25 @@ import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.xstopho.resourceconfigapi.config.ModConfig;
 import net.xstopho.resourceconfigapi.gui.widget.value_list.ValueListWidget;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigListWidget extends ObjectSelectionList<ConfigListEntry> {
 
     private final ValueListWidget valueListWidget;
+    private final List<ConfigListEntry> valueEntries = new ArrayList<>();
 
-    public ConfigListWidget(Minecraft client, int width, int height, int yPos, int itemHeight, List<ModConfig> configList, ValueListWidget valueListWidget) {
-        super(client, width, height, yPos, itemHeight);
+    public ConfigListWidget(int width, int height, int yPos, int itemHeight, List<ModConfig> configList, ValueListWidget valueListWidget) {
+        super(Minecraft.getInstance(), width, height, yPos, itemHeight);
         this.valueListWidget = valueListWidget;
 
-        configList.forEach(this::addConfigEntries);
+        configList.forEach(this::processConfigs);
     }
 
-    private void addConfigEntries(ModConfig config) {
-        this.addEntry(new ConfigListEntry(config, this.valueListWidget));
+    private void processConfigs(ModConfig config) {
+        ConfigListEntry entry = new ConfigListEntry(config, this.valueListWidget);
+        this.valueEntries.add(entry);
+        this.addEntry(entry);
     }
 
     @Override
@@ -30,5 +34,9 @@ public class ConfigListWidget extends ObjectSelectionList<ConfigListEntry> {
     @Override
     public int getRowWidth() {
         return this.width - 4;
+    }
+
+    public List<ConfigListEntry> getValueEntries() {
+        return valueEntries;
     }
 }
