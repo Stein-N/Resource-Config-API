0e81956c0ddaf6c2c8ef63a8fcf0896ad51fec3b
Stein-N <nico-stein-98@outlook.de>
2024-12-20 16:25:26 +0100

    adding translation value to ConfigEntry annotation

--- common/src/main/java/net/xstopho/resourceconfigapi/annotations/ConfigEntry.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/annotations/ConfigEntry.java
@@ -7,4 +7,15 @@ import java.lang.annotation.RetentionPolicy;
 public @interface ConfigEntry {
 
     String category() default "";
+
+    /**
+     * Define a custom translation key, this is useful when multiple config settings
+     * have the same purpose but f.e. different Tiers. <br>
+     * Only set f.e. maxExtract.<br>
+     * The key will be fully generated when the
+     * {@link net.xstopho.resourceconfigapi.gui.widget.config_list.ConfigListEntry}
+     * initialized the ValueEntries
+     * @return Translation key
+     */
+    String translation() default "";
 }
