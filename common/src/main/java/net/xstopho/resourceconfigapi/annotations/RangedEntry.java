2a39a38328620d6174077678ee9d9951380c8513
Stein-N <nico-stein-98@outlook.de>
2024-12-23 22:10:04 +0100

    remove default value from RangedEntry annotation

--- common/src/main/java/net/xstopho/resourceconfigapi/annotations/RangedEntry.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/annotations/RangedEntry.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RangedEntry {
 
-    float minValue() default -1f;
-    float maxValue() default -1f;
+    float minValue();
+    float maxValue();
 }
