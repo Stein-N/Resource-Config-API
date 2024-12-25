fa0b99347a1c035439d0a2591fbade0a4ad30611
Stein-N <nico-stein-98@outlook.de>
2024-12-26 00:09:26 +0100

    make the RangedEntrySlider more reliable and usable

--- common/src/main/java/net/xstopho/resourceconfigapi/gui/widget/RangedEntrySlider.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/gui/widget/RangedEntrySlider.java
@@ -3,35 +3,64 @@ package net.xstopho.resourceconfigapi.gui.widget;
 import net.minecraft.client.gui.components.AbstractSliderButton;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
-import net.xstopho.resourceconfigapi.Constants;
+
+import java.util.function.Consumer;
 
 public class RangedEntrySlider extends AbstractSliderButton {
 
-    private final double minValue;
-    private final double maxValue;
+    private final double minValue, maxValue, currentValue;
 
     private final boolean integer;
 
-    public RangedEntrySlider(int x, int y, int width, int height, Component message, double currentValue, double minValue, double maxValue, boolean integer) {
-        super(x, y, width, height, message, currentValue / maxValue);
+    private Consumer<Double> responder;
+
+    public RangedEntrySlider(int x, int y, int width, int height, double currentValue, double minValue, double maxValue, boolean integer) {
+        super(x, y, width, height, Component.empty(), currentValue / maxValue);
+        this.currentValue = currentValue;
         this.minValue = minValue;
         this.maxValue = maxValue;
 
         this.integer = integer;
+
+        updateMessage();
     }
 
     @Override
     protected void updateMessage() {
-        this.setMessage(Component.literal(String.valueOf(getValue())));
+        if (integer) {
+            setMessage(Component.literal(String.valueOf((int) getValue())));
+            return;
+        }
+        setMessage(Component.literal(String.valueOf(getValue())));
     }
 
     @Override
     protected void applyValue() {
-        Constants.LOG.error("Current Value: {}", getValue());
+        if (responder != null) {
+            this.responder.accept(getValue());
+        }
+    }
+
+    public void setResponder(Consumer<Double> consumer) {
+        this.responder = consumer;
     }
 
     public double getValue() {
-        double current = Mth.clampedLerp(this.minValue, this.maxValue, this.value);
-        return this.integer ? (int) Math.round( current) : (double) Math.round(current * 100) / 100;
+        double clampedValue = Mth.clampedLerp(this.minValue, this.maxValue, this.value);
+        if (maxValue > 1) {
+            clampedValue = Math.round(clampedValue / 0.10) * 0.10;
+        }
+        return this.integer ? (int) Math.round(clampedValue) : (double) Math.round(clampedValue * 100) / 100;
+    }
+
+    public void undoValue() {
+        this.value = currentValue / maxValue;
+        updateMessage();
+    }
+
+    public void setValue(double doubleValue) {
+        this.value = doubleValue / maxValue;
+        updateMessage();
+        applyValue();
     }
 }
