c6625a97cbd563bcf751f4d2374b0ae54ca6f2f7
Stein-N <nico-stein-98@outlook.de>
2024-12-27 15:27:55 +0100

    adding payload

---
+++ fabric/src/main/java/net/xstopho/resourceconfigapi/network/server/OperatorStatusPayload.java
@@ -0,0 +1,26 @@
+package net.xstopho.resourceconfigapi.network.server;
+
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.xstopho.resourceconfigapi.ClientConstants;
+import net.xstopho.resourceconfigapi.Constants;
+
+public record OperatorStatusPayload(boolean status) implements CustomPacketPayload {
+    public static final Type<OperatorStatusPayload> TYPE = new Type<>(Constants.of("operator_status_payload"));
+    public static final StreamCodec<RegistryFriendlyByteBuf, OperatorStatusPayload> CODEC =
+            StreamCodec.composite(ByteBufCodecs.BOOL, OperatorStatusPayload::status, OperatorStatusPayload::new);
+
+    public static void handle(OperatorStatusPayload payload, ClientPlayNetworking.Context context) {
+        context.client().execute(() -> {
+            ClientConstants.isOperator = payload.status();
+        });
+    }
+
+    @Override
+    public Type<? extends CustomPacketPayload> type() {
+        return TYPE;
+    }
+}
