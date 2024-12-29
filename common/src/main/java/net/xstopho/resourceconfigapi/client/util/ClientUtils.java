673fa12765fe39d98b50694bd9f3a6245573afc2
Stein-N <nico-stein-98@outlook.de>
2024-12-29 14:31:23 +0100

    add isOperator check to ClientUtils#sendConfigUpdateToServer instead of ResourceConfigScreen#saveConfigChanges

--- common/src/main/java/net/xstopho/resourceconfigapi/client/util/ClientUtils.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/client/util/ClientUtils.java
@@ -3,12 +3,14 @@ package net.xstopho.resourceconfigapi.client.util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.ServerData;
+import net.xstopho.resourceconfigapi.client.ClientConstants;
 import net.xstopho.resourceconfigapi.network.NetworkHook;
 import net.xstopho.resourceconfigapi.platform.CoreServices;
 
 public class ClientUtils {
 
     public static void sendConfigUpdateToServer(String file, String json) {
+        if (!ClientConstants.isOperator) return;
         CoreServices.load(NetworkHook.class).sendConfigUpdateToServer(file, json);
     }
 
