3e979200399a9a7d3bf04f855845238e22a41910
Stein-N <nico-stein-98@outlook.de>
2024-12-29 14:05:27 +0100

    add better javadoc for ConfigEntry#translation

--- common/src/main/java/net/xstopho/resourceconfigapi/annotations/ConfigEntry.java
+++ common/src/main/java/net/xstopho/resourceconfigapi/annotations/ConfigEntry.java
@@ -15,11 +15,30 @@ public @interface ConfigEntry {
     String category() default "";
 
     /**
-     * Define a custom translation key, this is useful when multiple config settings
-     * have the same purpose but f.e. different Tiers. <br>
-     * Only set f.e. maxExtract.<br>
-     * The key will be fully generated when the {@link ConfigListEntry} initialized the ValueEntries
-     * @return Translation key
+     * Define a unified translation key for multiple ConfigEntries.<br>
+     * This key will be used for the label and tooltip translation key.<br>
+     * Example:<br>
+     * You have 3 Solar Panels with the same Config options, here are only the ConfigEntries for the Basic,
+     * but there will be the same options for advanced and industrial.
+     * <pre>{@code
+     * @Config(fileName = "solar panels", type = ConfigType.COMMON)
+     * public class SolarPanels {
+     *      @ConfigEntry(category = "Basic Solar Panel")
+     *      public static int basicTier = 1;
+     *
+     *      @ConfigEntry(category = "Basic Solar Panel")
+     *      public static int basicGenerateByDay = 25;
+     *
+     *      @ConfigEntry(category = "Basic Solar Panel")
+     *      public static int basicGenerateByNight = 3;
+     * }
+     * }</pre>
+     *
+     * When you now set a generic Translation key like: tier, generateByDay and generateByNight <br>
+     * You only have to add the translation once instead of 3 times, this reduces the
+     * added translation keys from 9 down to 3
+     *
+     * @return Translation key used for Label and Tooltip
      */
     String translation() default "";
 }
