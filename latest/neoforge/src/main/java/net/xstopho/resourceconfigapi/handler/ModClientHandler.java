53d6cf8cd32e607be9e664def5d0fc5009909a4f
Stein-N <nico-stein-98@sbvl.de>
2025-10-04 12:00:38 +0200

    Revert "remove screen factories from neo/-forge"
    This reverts commit 770ed6686579af4d0f3d24fef630bdc968b596e2.

---
+++ latest/neoforge/src/main/java/net/xstopho/resourceconfigapi/handler/ModClientHandler.java
@@ -0,0 +1,39 @@
+package net.xstopho.resourceconfigapi.handler;
+
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.client.network.event.RegisterClientPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+import net.xstopho.resourceconfigapi.ConfigConstants;
+import net.xstopho.resourceconfigapi.api.ConfigRegistry;
+import net.xstopho.resourceconfigapi.client.gui.screen.ResourceConfigScreen;
+import net.xstopho.resourceconfigapi.config.ModConfig;
+
+import java.util.Map;
+import java.util.Optional;
+
+@EventBusSubscriber(modid = ConfigConstants.MOD_ID, value = Dist.CLIENT)
+public class ModClientHandler {
+
+    @SubscribeEvent
+    public static void registerModConfigScreens(FMLClientSetupEvent event) {
+        for (Map.Entry<ResourceLocation, ModConfig> entry : ConfigRegistry.getConfigEntries()) {
+            Optional<? extends ModContainer> container = ModList.get().getModContainerById(entry.getKey().getNamespace());
+
+            container.ifPresent(modContainer -> {
+                ModLoadingContext.get().setActiveContainer(modContainer);
+                ModLoadingContext.get().registerExtensionPoint(
+                        IConfigScreenFactory.class,
+                        () -> (modContainer1, screen) -> new ResourceConfigScreen(screen, modContainer1.getModId())
+                );
+            });
+        }
+    }
+}
